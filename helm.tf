5cd03f8d5a2bbfeee5480a10df0f8406b3c23dae
Andrey Olishchuk <bobroe.web@gmail.com>
2022-07-11 10:48:34 +0200

    argocd app

--- helm.tf
+++ helm.tf
@@ -11,7 +11,47 @@ resource "null_resource" "wait_for_cluster" {
 
 resource "helm_release" "argocd" {
   name       = "argocd"
+  namespace  = "argocd"
   repository = "https://argoproj.github.io/argo-helm"
   chart      = "argo-cd"
-  depends_on = [null_resource.wait_for_cluster]
+  depends_on = [null_resource.wait_for_cluster, kubernetes_namespace.argocd]
+}
+
+resource "kubectl_manifest" "argoapp1" {
+  depends_on = [helm_release.argocd]
+  yaml_body = <<YAML
+apiVersion: argoproj.io/v1alpha1
+kind: Application
+metadata:
+  name: guestbook
+  namespace: argocd
+spec:
+  project: default
+  source:
+    repoURL: https://github.com/argoproj/argocd-example-apps.git
+    targetRevision: HEAD
+    path: guestbook
+  destination:
+    server: https://kubernetes.default.svc
+    namespace: guestbook
+  syncPolicy:
+    syncOptions:
+    - CreateNamespace=true
+    automated:
+      selfHeal: true
+      prune: true 
+      allowEmpty: false 
+YAML
+}
+
+resource "kubernetes_namespace" "argocd" {
+  metadata {
+    name = "argocd"
+  }
+}
+
+resource "kubernetes_namespace" "guestbook" {
+  metadata {
+    name = "guestbook"
+  }
 }
